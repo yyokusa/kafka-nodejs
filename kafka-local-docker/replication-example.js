d14dd06701fa99795d009ec87cb1b49e8b6c7b9d
Yasin A. Yokus <ayokus@icloud.com>
2023-07-30 16:08:59 +0300

    fix: provide inter broker protocol through KAFKA_INTER_BROKER_LISTENER_NAME

--- kafka-local-docker/replication-example.js
+++ kafka-local-docker/replication-example.js
@@ -4,7 +4,7 @@ const admin = Kafka.AdminClient.create({
     'bootstrap.servers': 'localhost:9091,localhost:9092,localhost:9093',
 });
 
-const topicName = 'cool-topic-1000';
+const topicName = 'cool-topic-1010';
 
 const newTopic = {
     topic: topicName,
@@ -19,52 +19,74 @@ const newTopic = {
 admin.createTopic(newTopic, (err) => {
         if (err)
             console.log("error creating topic: ", err);
-        else
+        else {
             console.log("topic created: ", topicName);
+
+            // create consumer client
+            const consumer = new Kafka.KafkaConsumer({
+                'group.id': 'kafka',
+                'metadata.broker.list': 'localhost:9091,localhost:9092,localhost:9093',
+            }, {'auto.offset.reset': 'earliest'});
+
+            consumer.on('ready', (arg) => {
+                console.log('consumer ready.' + JSON.stringify(arg));
+                consumer.subscribe([topicName])
+                //start consuming messages
+                consumer.consume();
+
+                consumer.getMetadata({
+                    topic: topicName,
+                    timout: 1000
+                }, (err, metadata) => {
+                    if (err) {
+                        console.error('Error getting metadata');
+                        console.error(err);
+                    } else {
+                        console.log('Metadata', metadata);
+                        // print the topics and corresponding partitions
+                        metadata.topics.forEach(topic => {
+                            if (topic.name !== '__consumer_offsets') {
+                                console.log('Topic: ', topic.name);
+                                topic.partitions.forEach(partition => console.log('Partition: ', partition));
+                            }
+                        });
+                    }
+                });
+            });
+            // get all topics
+            consumer.connect();
+        }
     }
 );
 
-// create consumer client
-const consumer = new Kafka.KafkaConsumer({
-    'group.id': 'kafka',
-    'metadata.broker.list': 'localhost:9091,localhost:9092,localhost:9093',
-}, {'auto.offset.reset': 'earliest'});
-
-consumer.on('ready', (arg) => {
-    console.log('consumer ready.' + JSON.stringify(arg));
-    consumer.subscribe([topicName])
-    //start consuming messages
-    consumer.consume();
-
-    consumer.getMetadata({
-        topic: topicName,
-        timout: 1000
-    }, (err, metadata) => {
-        if (err) {
-            console.error('Error getting metadata');
-            console.error(err);
-        } else {
-            console.log('Metadata', metadata);
-        }
-    });
-});
-// get all topics
-consumer.connect();
-
-// metadata:  {
-//     orig_broker_id: 2,
-//     orig_broker_name: 'localhost:9092/2',
-//     topics: [
-//       { name: 'cool-topic', partitions: [Array] },
-//       { name: '__consumer_offsets', partitions: [Array] }
-//     ],
-//     brokers: [
-//       { id: 2, host: 'localhost', port: 9092 },
-//       { id: 3, host: 'localhost', port: 9093 },
-//       { id: 1, host: 'localhost', port: 9091 }
-//     ]
-//   }
-//   partitions:  [
-//     { id: 0, leader: 2, replicas: [ 2, 1, 3 ], isrs: [ 2 ] },
-//     { id: 1, leader: 3, replicas: [ 3, 2, 1 ], isrs: [ 3, 2, 1 ] }
+// node replication-example.js
+// topic created:  cool-topic-1010
+// consumer ready.{"name":"rdkafka#consumer-2"}
+// Metadata {
+//   orig_broker_id: 1,
+//   orig_broker_name: 'localhost:9091/1',
+//   topics: [
+//     { name: 'cool-topic-1001', partitions: [Array] },
+//     { name: 'cool-topic-1004', partitions: [Array] },
+//     { name: 'cool-topic-1010', partitions: [Array] },
+//     { name: 'cool-topic-1002', partitions: [Array] },
+//     { name: '__consumer_offsets', partitions: [Array] }
+//   ],
+//   brokers: [
+//     { id: 2, host: 'localhost', port: 9092 },
+//     { id: 3, host: 'localhost', port: 9093 },
+//     { id: 1, host: 'localhost', port: 9091 }
 //   ]
+// }
+// Topic:  cool-topic-1001
+// Partition:  { id: 0, leader: 2, replicas: [ 2, 3, 1 ], isrs: [ 2, 3, 1 ] }
+// Partition:  { id: 1, leader: 3, replicas: [ 3, 1, 2 ], isrs: [ 3, 1, 2 ] }
+// Topic:  cool-topic-1004
+// Partition:  { id: 0, leader: 1, replicas: [ 1, 2, 3 ], isrs: [ 1, 2, 3 ] }
+// Partition:  { id: 1, leader: 2, replicas: [ 2, 3, 1 ], isrs: [ 2, 3, 1 ] }
+// Topic:  cool-topic-1010
+// Partition:  { id: 0, leader: 3, replicas: [ 3, 1, 2 ], isrs: [ 3, 1, 2 ] }
+// Partition:  { id: 1, leader: 1, replicas: [ 1, 2, 3 ], isrs: [ 1, 2, 3 ] }
+// Topic:  cool-topic-1002
+// Partition:  { id: 0, leader: 3, replicas: [ 3, 1, 2 ], isrs: [ 3, 1, 2 ] }
+// Partition:  { id: 1, leader: 1, replicas: [ 1, 2, 3 ], isrs: [ 1, 2, 3 ] }
